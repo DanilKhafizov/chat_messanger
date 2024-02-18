899fb424df2377268194994d3f75c383c2824b7e
Danil <danilhafizov123kk@gmail.com>
2024-02-18 22:51:29 +0500

    Мобильное приложение "Чат мессенджер"

---
+++ app/src/main/java/com/linar/chatapp/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.linar.chatapp.listeners;
+
+import com.linar.chatapp.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/linar/chatapp/listeners/UserListener.java
@@ -0,0 +1,8 @@
+package com.linar.chatapp.listeners;
+
+import com.linar.chatapp.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+    
+}
